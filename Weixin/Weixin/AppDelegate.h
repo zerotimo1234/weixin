af918b9efc0c7b659bf80cb4b5c603e8fb723ed4
ygj <ygj@163.com>
2016-07-10 11:50:14 +0800

    提交初始化代码

---
+++ Weixin/Weixin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weixin
+//
+//  Created by Yazi on 16/7/10.
+//  Copyright © 2016年 Yazi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weixin/Weixin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Weixin
+//
+//  Created by Yazi on 16/7/10.
+//  Copyright © 2016年 Yazi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
