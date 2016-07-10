4ea3f6595e9f0ad26e3afeef252247ac61df422f
ygj <ygj@163.com>
2016-07-10 11:55:29 +0800

    创建了一个person类

---
+++ Weixin/Weixin/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  Weixin
+//
+//  Created by Yazi on 16/7/10.
+//  Copyright © 2016年 Yazi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ Weixin/Weixin/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Weixin
+//
+//  Created by Yazi on 16/7/10.
+//  Copyright © 2016年 Yazi. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
